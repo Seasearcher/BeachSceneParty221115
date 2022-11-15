ecb88af4e1f80bd8136dd33ae845ea647391dc73
Seasearcher <113310219+Seasearcher@users.noreply.github.com>
2022-11-15 22:29:07 +0100

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1588258.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 110550108, 110550101, 110550093, 110550098, 110549891 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/110550018/1/basis.wasm.js', 'wasmUrl' : 'files/assets/110550105/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/110549955/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/110549999/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/110550084/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/110549896/1/ammo.js', 'preload' : true},
+];
